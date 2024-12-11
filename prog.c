7c7c5b6ee6e1f70a9f3b93d55542e92ce44df286
AugustinAveline <augustin.aveline@gmail.com>
2024-12-11 18:08:59 +0100

    Update prog.c
    mise en tant que le if pour pouvoir mettre des valeur au pife

--- prog.c
+++ prog.c
@@ -6,26 +6,16 @@ int main(){
     unsigned int a, b, c;
     type_Utilisateur d = station;
 
+    while(scanf("%d;%u;%u;%u", &d, &a, &b, &c) == 4){
+        printf("a = %u\n", a);
+        printf("b = %u\n", b);
+        printf("c = %u\n", c);
+        printf("d = %u\n", d);
+        if(scanf("%d;%u;%u;%u", &d, &a, &b, &c) != 4){
+            fprintf(stderr, "Error reading input\n");
+            return 1;
+        }
+    }
     
-    if(scanf("%d;%u;%u;%u", &d, &a, &b, &c) != 4){
-        fprintf(stderr, "Error reading input\n");
-        return 1;
-    }
-
-    printf("a = %u\n", a);
-    printf("b = %u\n", b);
-    printf("c = %u\n", c);
-    printf("d = %u\n", d);
-
-    if(scanf("%d;%u;%u;%u", &d, &a, &b, &c) != 4){
-        fprintf(stderr, "Error reading input\n");
-        return 1;
-    }
-
-    printf("a* = %u\n", a);
-    printf("b = %u\n", b);
-    printf("c = %u\n", c);
-    printf("d = %d\n", d);
-
     return 0;    
 }
