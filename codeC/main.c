140428a55a8c3ed2aefb0c0387a89b780723f725
AugustinAveline <augustin.aveline@gmail.com>
2024-12-19 11:30:52 +0100

    Update main.c

--- codeC/main.c
+++ codeC/main.c
@@ -14,4 +14,18 @@ int main()
     printf("2:%d:%d\n",30-i,i);
   }
   return 0;
+
+
+
+
+
+
+    // srand(time(NULL));
+    // pAVL avl = NULL;
+    // avl=transfert_donné_ds_AVL(avl);
+    // afficherAVL(avl,0);
+    // printf("\n\n\n");
+    // affichageinfixe(avl);
+    // freeAVL(avl);
+    // return 0;
 }
