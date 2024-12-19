808ea1600b5884a778498d958dc3a93a500c7451
AugustinAveline <augustin.aveline@gmail.com>
2024-12-19 14:35:48 +0100

    Update avlUtile.h

--- codeC/avlUtile.h
+++ codeC/avlUtile.h
@@ -1,8 +1,8 @@
+#pragma once
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <assert.h>
-#pragma once
 
 void afficherAVL(pAVL nd, int niveau);
 void affichageinfixe(pAVL noeuds);
