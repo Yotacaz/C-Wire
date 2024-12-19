e045a84eb94b3a7aacdc849e63faa4d68a23b4d6
AugustinAveline <augustin.aveline@gmail.com>
2024-12-19 14:23:19 +0100

    Update station.c

--- codeC/station.c
+++ codeC/station.c
@@ -1,6 +1,6 @@
 #include "station.h"
 
-/fonction qui renvoie les données au shell sous la bonne forme
+//fonction qui renvoie les données au shell sous la bonne forme
 void traiter_station(Donnee_station station){
     printf("%lu:%lu:%lu\n",station.ID_station, station.capacite, station.consomation);
 }
