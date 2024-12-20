4c435154d024df7a335f1bed005d51ae0c538c8e
Yotacaz <165938795+Yotacaz@users.noreply.github.com>
2024-12-20 09:15:34 +0000

    optimisation de la fonction traiter_station

--- codeC/station.c
+++
@@ -1,7 +0,0 @@
-#include "station.h"
-
-// fonction qui renvoie les données au shell sous la bonne forme
-void traiter_station(Donnee_station station) {
-    printf("%lu:%lu:%lu\n", station.ID_station, station.capacite,
-           station.conso);
-}
--- codeC/station.h
+++ codeC/station.h
@@ -8,4 +8,5 @@ typedef struct _donneeStation // raccourcie qui ralonge
     unsigned long capacite;
 } Donnee_station;
 
-void traiter_station(Donnee_station station);
+// fonction qui renvoie les données au shell sous la bonne forme
+#define traiter_station(station) printf("%lu:%lu:%lu\n",station.ID_station, station.capacite, station.conso)
