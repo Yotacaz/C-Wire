28b4a36d999c9f3a4320bb3d2d9c6904f035a87f
Yotacaz <165938795+Yotacaz@users.noreply.github.com>
2024-12-21 11:12:24 +0000

    commentaire

--- codeC/main.c
+++ codeC/main.c
@@ -11,9 +11,13 @@ int main(int argc, char const *argv[]) {
     assert(argc == 2);
     const char* nom_fichier = argv[1];
     assert(nom_fichier);
+
+    //traitement des données
     pAVL avl = NULL;
     avl = transfert_donne_ds_AVL(avl);
     assert(avl);    //Il doit y avoir au moins une station dans l'AVL
+
+    //affichage des données (et écriture dans un fichier si un nom de fichier est passé en argument)
     affichage_infixe(avl);
 
     //Si un fichier est passé en argument, on écrit l'arbre avec
